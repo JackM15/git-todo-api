59807724228b0083621b3f9aff1e140739efc9ce
Jack <jackm550@googlemail.com>
2017-08-01 10:27:36 +0100

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -2,20 +2,22 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-//local imports
+//local imports (es6 destructuring)
 var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
 
-//create express app variable
+//create express app
 var app = express();
-//set express to use bodyparser which enables it to send json data
+//set express to use bodyparser
 app.use(bodyParser.json());
 
 //routes
+//post request for todos, send info over to db.
 app.post("/todos", (req, res) => {
     //create new Todo object/model from body text
     var todo = new Todo({
+        //grab the text data from the parsed body and set the value = text in our model.
         text: req.body.text
     });
 
@@ -27,7 +29,10 @@ app.post("/todos", (req, res) => {
     });
 });
 
-//start server (so it listens for connections and handles them)
+//start server
 app.listen(3000, () => {
     console.log("Started on port 3000.")
 });
+
+//export app
+module.exports = {app};
